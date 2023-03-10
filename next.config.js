6ec7ecffa4d2feedfc559aa2055687caf53e1f56
GulsahDgr <34721537+GulsahDgr@users.noreply.github.com>
2023-03-10 14:10:40 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  //This part is added to avoid file system Node.js module error - 14.02.2023 - BEGINNING -- BY DEVELOPER GULSAH
+  webpack(config, { nextRuntime }) {
+    // as of Next.js latest versions, the nextRuntime is preferred over `isServer`, because of edge-runtime
+    if (typeof nextRuntime === "undefined") {
+      const { IgnorePlugin } = require("webpack");
+      const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+      config.plugins.push(ignoreFs);
+    }
+
+    return config;
+  },
+  //This part is added to avoid file system Node.js module error - 14.02.2023 - END -- BY DEVELOPER GULSAH
+
 }
 
 module.exports = nextConfig
